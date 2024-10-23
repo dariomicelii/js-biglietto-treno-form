489d7395472f0c78d82f18ff5b8f136e9f92fb2c
dariomicelii <dariomiceli01@gmail.com>
2024-10-23 17:30:12 +0200

    situazione attuale js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,17 @@ const askKm = document.getElementById("ask-km");
 const askAge = document.getElementById("ask-age"); 
 const generateButton = document.getElementById("generate-button");
 const deleteButton = document.getElementById("delete-button");
+const outputBiglietto = document.getElementById("output-biglietto")
 
+function randomCarrozza(min, max) {
+    const carrozza = Math.floor(Math.random() * 6 + 1); 
+    return carrozza
+}
+
+function randomCP(min, max) {
+    const cp = Math.floor(Math.random() * 100000 + 1); 
+    return cp
+}
 
 generateButton.addEventListener("click", () => {
 const passengerName = askName.value
@@ -15,30 +25,60 @@ console.log(userAge);
 
 const maggioreEta = 18
 let over65 = 65
-
+let discount = 0;
 let ticketPrice = Math.round(numKilom * 0.21)
 
 // SE eta è minore di 18 
 if(userAge === "Minorenne"){
-    const youngDiscount = (ticketPrice * 20) / 100;
-    message = "Il prezzo del tuo biglietto è di:" + " " + ticketPrice.toString() + "Euro, Hai diritto ad uno sconto del 20%! Ecco il prezzo scontato del tuo biglietto:" + " " + youngDiscount.toFixed(2)
+    discount = (ticketPrice * 20) / 100;
+    message = "Il prezzo del tuo biglietto è di:" + " " + ticketPrice.toString() + "Euro, Hai diritto ad uno sconto del 20%! Ecco il prezzo scontato del tuo biglietto:" + " " + discount.toFixed(2)
+    offerta = "Minorenni"
 }
 
 // ALTRIMENTI SE eta è maggiore di 65
 else if(userAge === "Over65"){
-    const over65Discount = (ticketPrice * 40) / 100;
-    message = " Il prezzo del tuo biglietto è di:" + " " + ticketPrice.toString() + "Euro, Hai diritto ad uno sconto del 40%! Ecco il prezzo scontato del tuo biglietto:" + " " + over65Discount.toFixed(2) 
+    discount = (ticketPrice * 40) / 100;
+    message = " Il prezzo del tuo biglietto è di:" + " " + ticketPrice.toString() + "Euro, Hai diritto ad uno sconto del 40%! Ecco il prezzo scontato del tuo biglietto:" + " " + discount.toFixed(2) 
+    offerta = "Over65"
 }
 
 else if(userAge === "Maggiorenne"){
     message = "Non hai diritto ad alcuno sconto =(, il prezzo del tuo biglietto è di:" + " " + ticketPrice.toFixed(2) + "Euro" 
+    offerta = "Standard"
 }
 
 else{
     message = "Ooops, il numero che hai inserito non è valido =("
 }
 
-console.log(message)
+
+//! JS GENERAZIONE BIGLIETTO
+const ilTuoBiglietto =`
+<div class="col-4">
+  <h2>NOME PASSEGGERO</h2>
+</div>
+<div class="col-8">
+  <div class="row">
+    <div class="col-2">
+        <h3>Offerta</h3>
+        <div>${offerta}</div>
+    </div>
+    <div class="col-2">
+        <h3>Carrozza</h3>
+        <div>${randomCarrozza(1, 6)}</div>
+    </div>
+    <div class="col-2">
+        <h3>Codice CP</h3>
+        <div>${randomCP(1, 100000)}</div>
+    </div>
+    </div class="col-2>
+        <h3>Sconto</h3>
+        <div class="col-2">${discount}</div>
+    </div>
+  </div>
+</div>`;
+
+outputBiglietto.innerHTML = ilTuoBiglietto
 });
 
 
